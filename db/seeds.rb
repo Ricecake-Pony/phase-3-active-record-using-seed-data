519fa27e5e5e5da30398521c1d892386170409d3
Trey <trey.delcamp@gmail.com>
2022-11-28 08:18:32 -0600

    commit tree 20262ad20872c58bebdc2f54a11c7dd8aab2ba1f

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,14 @@
+puts "Clearing db"
+Game.destroy_all
+puts "Database Cleared"
+
+puts "creating the game!"
+50.times do
+    Game.create(
+    title: Faker::Game.title,
+    genre: Faker::Game.genre,
+    platform: Faker::Game.platform,
+    price: rand(0..60))
+end
+puts "Games are ready for listing!"
 # Add a console message so we can see output when the seed file runs
